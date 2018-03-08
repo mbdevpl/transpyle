e3f337c2c18c35f887ae7acb6b313914ded9daa4
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-03-08 14:52:07 +0900

    minor refactoring

--- transpyle/main.py
+++ transpyle/main.py
@@ -22,17 +22,29 @@ def query_registry() -> pd.DataFrame:
                         data=[support for _, support in language_support.items()], dtype=bool)
 
 
-def main(args=None, namespace=None):
-    """Parse commandline arguments and execute transpyle accordingly."""
+def show_supported_langs():
+    support_data = query_registry()
+    support_data = pd.DataFrame(
+        columns=[cls.__name__ for cls in support_data.columns],
+        index=[language.default_name for language in support_data.index],
+        data=[['✓' if support else '' for support in data_row]
+              for _, data_row in support_data.iterrows()], dtype=str)
+    print(support_data.to_string(header=list(step_descriptions.keys()), justify='center'))
+    print()
+    print('caution: for each supported language, only a certain subset of syntax'
+          ' and functionality is supported')
 
+
+def parse_args(args=None):
+    """Parse commandline arguments of transpyle CLI."""
     prog_name = 'transpyle'
-    copyright_notice = 'Copyright 2017 Mateusz Bysiek https://mbdevpl.github.io/,' \
+    copyright_notice = 'Copyright 2017-2018 Mateusz Bysiek https://mbdevpl.github.io/,' \
         ' Apache License 2.0'
 
     parser = argparse.ArgumentParser(
         prog=prog_name,
         description='Human-oriented and HPC-oriented transpiler.',
-        epilog=copyright_notice)
+        epilog=copyright_notice, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('source', nargs=('?' if len(sys.argv) > 1 else None), help='source path')
     parser.add_argument('target', nargs='?',
                         help='target path, print results to stdout if not provided')
@@ -44,14 +56,28 @@ def main(args=None, namespace=None):
                         help='programming language to transpile from, detected if not provided')
     parser.add_argument('--to-language', '--to', type=str, default=None,
                         help='programming language to transpile to, detected from target path')
-    parser.add_argument('--objects', '--object', '--obj', type=str, default=None,
+    parser.add_argument('--scopes', '--scope', metavar='slice', type=slice, nargs='*', default=None,
+                        help='only given line scopes are transpiled')
+    parser.add_argument('--objects', '--object', '--obj', metavar='pattern', type=str, nargs='*',
+                        default=None,
                         help='transpile only selected object(s) within the file (acceps regular'
                         ' expressions), whole file is transpiled if not provided')
+    parser.add_argument('--keep', action='store_true',
+                        help='do not discard not-transpiled scopes of the code')
+    parser.add_argument('--transformations',
+                        help='specify Python script that defines performed AST transformations')
 
-    args = parser.parse_args(args, namespace)
+    parsed_args = parser.parse_args(args)
 
-    if args.help_languages:
-        support_data = query_registry()
+    return parsed_args
+
+
+def main(args=None):
+    """Parse commandline arguments and execute transpyle accordingly."""
+
+    parsed_args = parse_args(args)
+
+    if parsed_args.help_languages:
         step_descriptions = {
             'parsing':
             'transforming code in a specific programming language into language-specific AST',
@@ -63,46 +89,47 @@ def main(args=None, namespace=None):
             'compiling':
             'transforming source code in a specific programming language into compiled binary',
             'binding': 'creating a callable Python object for a compiled library'}
-        support_data = pd.DataFrame(
-            columns=[cls.__name__ for cls in support_data.columns],
-            index=[language.default_name for language in support_data.index],
-            data=[['✓' if support else '' for support in data_row]
-                  for _, data_row in support_data.iterrows()], dtype=str)
         print('transpilation step support for each supported language')
         print()
         for step, desc in step_descriptions.items():
             print('{}:'.format(step))
             print('  {}'.format(desc))
         print()
-        print(support_data.to_string(header=list(step_descriptions.keys()), justify='center'))
-        print()
-        print('caution: for each supported language, only a certain subset of syntax'
-              ' and functionality is supported')
+        show_supported_langs()
         print()
         print('{}, {}'.format(prog_name, copyright_notice))
         return
 
-    if args.source is None:
+    if parsed_args.source is None:
         raise NotImplementedError('source path was not provided')
 
-    if args.target is None:
+    if parsed_args.target is None:
         raise NotImplementedError('printing to stdout not supported yet')
 
-    if any(_ is None for _ in (args.from_language, args.to_language)):
+    if any(_ is None for _ in (parsed_args.from_language, parsed_args.to_language)):
         raise NotImplementedError('language detection not supported yet')
 
-    if args.objects is not None:
+    if parsed_args.objects is not None:
         raise NotImplementedError('object selection not supported yet')
 
-    from_language = Language.find(args.from_language)
-    to_language = Language.find(args.to_language)
+    if parsed_args.scopes is not None:
+        raise NotImplementedError('code scope selection not supported yet')
+
+    if parsed_args.keep:
+        raise NotImplementedError('--keep option not suppored yet')
+
+    if parsed_args.transformations is not None:
+        raise NotImplementedError('--trasformations option not suppored yet')
+
+    from_language = Language.find(parsed_args.from_language)
+    to_language = Language.find(parsed_args.to_language)
 
     reader = CodeReader(from_language.file_extensions)
     translator = AutoTranslator(from_language, to_language)
     writer = CodeWriter(to_language.default_file_extension)
 
-    from_path = pathlib.Path(args.source)
-    to_path = pathlib.Path(args.target)
+    from_path = pathlib.Path(parsed_args.source)
+    to_path = pathlib.Path(parsed_args.target)
 
     from_code = reader.read_file(from_path)
     to_code = translator.translate(from_code, from_path)
