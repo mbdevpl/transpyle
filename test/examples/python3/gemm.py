8f3cdd281b9b1030bc9ce5e12ec81921b89cc261
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-06-20 19:47:52 +0900

    some examples were not compatible with Python 3.5

--- test/examples/python3/gemm.py
+++ test/examples/python3/gemm.py
@@ -7,9 +7,9 @@ import static_typing as st
 def dgemm(a: st.ndarray[2, np.double, (100, 100)],
           b: st.ndarray[2, np.double, (100, 100)]) -> st.ndarray[2, np.double, (100, 100)]:
 
-    y_max: np.int32 = a.shape(0)
-    i_max: np.int32 = a.shape(1)
-    x_max: np.int32 = b.shape(1)
+    y_max = a.shape(0)  # type: np.int32
+    i_max = a.shape(1)  # type: np.int32
+    x_max = b.shape(1)  # type: np.int32
     c: st.ndarray[2, np.double] = np.zeros((y_max, x_max), np.double)
 
     for y in range(y_max):  # type: np.int32
