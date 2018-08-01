a5deb4bd7a5b05394b20f6b0db12901301828564
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-08-01 10:37:58 +0900

    temporary fixes of fortran version

--- test/examples/python3/compute_pi.py
+++ test/examples/python3/compute_pi.py
@@ -3,9 +3,12 @@ import numpy as np
 
 
 def compute_pi(segments: int) -> np.double:
-    polygon_edge_length_squared = np.double(2.0)  # type: np.double
+    polygon_edge_length_squared = 2.0  # type: np.double
     polygon_sides = 2  # type: int
-    for _ in range(segments):  # type: int
+    # temporary fix of Fortran version
+    polygon_edge_length_squared = 2.0
+    polygon_sides = 2
+    for n in range(segments):  # type: int
         polygon_edge_length_squared = -np.sqrt(1 - polygon_edge_length_squared / 4) * 2 + 2
         polygon_sides *= 2
     return np.sqrt(polygon_edge_length_squared) * polygon_sides
