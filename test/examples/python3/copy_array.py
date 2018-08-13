1cdaf74a4e052fff136b51a7bcc142dfc40e32dd
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-08-12 19:08:39 -0500

    cleanup [skip ci]

--- test/examples/python3/copy_array.py
+++ test/examples/python3/copy_array.py
@@ -4,6 +4,7 @@ import static_typing as st
 
 N = st.generic.GenericVar()
 
+
 def copy_array(input_data: st.ndarray[1, np.double, (N,)]) -> st.ndarray[1, np.double, (N,)]:
     output_data = np.zeros((input_data.size,), dtype=np.double)
     # t y p e: st.ndarray[1, np.double] # TMP skipping because of return type information
