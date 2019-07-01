d06baa4758c1202665612ebe4d83d64a165fa535
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-07-01 13:32:40 +0900

    add some OpenACC pragmas

--- test/examples/python3/gemm.py
+++ test/examples/python3/gemm.py
@@ -12,9 +12,17 @@ def dgemm(a: st.ndarray[2, np.double, (100, 100)],
     xmax = b.shape(1)  # type: np.int32
     c: st.ndarray[2, np.double] = np.zeros((ymax, xmax), dtype=np.double)
 
+    # pragma: acc parallel copyin(a,b) copyout(c)
+    # pragma: acc loop gang
     for y in range(ymax):  # type: np.int32
+        # pragma: acc loop worker
         for i in range(imax):  # type: np.int32
+            # pragma: acc loop vector reduction(+: c[y][x])
             for x in range(xmax):  # type: np.int32
                 c[y, x] += a[y, i] * b[i, x]
+            # pragma: acc end loop
+        # pragma: acc end loop
+    # pragma: acc end loop
+    # pragma: acc end parallel
 
     return c
