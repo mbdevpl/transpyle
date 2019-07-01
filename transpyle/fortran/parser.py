88500c06d8b50eee1cd449cc8caf45edc867b6f9
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-07-01 13:32:12 +0900

    read xml before preprocessing

--- transpyle/fortran/parser.py
+++ transpyle/fortran/parser.py
@@ -14,5 +14,10 @@ class FortranParser(Parser):
     def _parse_scope(self, code: str, path: pathlib.Path = None) -> ET.Element:
         assert path is not None, path
         result = open_fortran_parser.execute_parser(path, None, verbosity=100)
+        try:
+            stdout_str = result.stdout.decode()
+            xml = ET.fromstring(stdout_str)
+        except ET.ParseError:
+            result.returncode = -1
         summarize_completed_process(result, executable=pathlib.Path('open_fortran_parser'))
-        return ET.fromstring(result.stdout)
+        return xml
